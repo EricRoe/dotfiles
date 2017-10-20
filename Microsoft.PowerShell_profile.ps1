711c64607dbae8e9439bdc030727dbf9ee3ec5a5
Eric Roe <eric.roe@outlook.com>
2017-10-20 14:39:07 -0700

    Cleaned up ps profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,15 @@
-set-location c:\projects
+######################################################################
 # Load posh-git example profile
-. 'C:\tools\poshgit\dahlbyk-posh-git-c39da78\profile.example.ps1'
 
+. 'C:\tools\poshgit\dahlbyk-posh-git-c39da78\profile.example.ps1'
+# Good enough
 Rename-Item Function:\Prompt PoshGitPrompt -Force
 function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
 
-$env:path += ";c:\Program Files\MongoDB\Server\3.2\bin"
+######################################################################
+# Helper functions
 
-set-alias vi 'C:\Program Files (x86)\vim\vim80\vim.exe'
-set-alias vim 'C:\Program Files (x86)\vim\vim80\vim.exe'
-set-alias man get-help -option allscope
-set-alias lsa ls-all -option allscope
-set-alias cdl cd-ls -option allscope 
-set-alias cls scroll-screen -option allscope
-
+# I should really learn how to write powershell better
 function alert($time=0)
 {
     sleep($time)
@@ -24,6 +20,7 @@ function edit-profile()
 {
     vi $profile
 }
+
 function ls-all($path)
 {
     $i = gci $path
@@ -37,6 +34,7 @@ function cd-ls($path)
     ls-all
 }
 
+# Consider moving to .gitconfig
 function gitl($n) 
 {
    if ($n) {
@@ -46,6 +44,7 @@ function gitl($n)
         git log --graph --all --decorate
     }
 }
+
 function touch($file)
 {
     echo $null >> $file
@@ -59,5 +58,20 @@ function scroll-screen($leaveRows){
         [system.console]::setwindowposition(0, [system.console]::cursortop)
     }
 }
+
+######################################################################
+# Startup stuff
+
+set-location c:\projects
 mode con cols=150 
 yacta
+
+######################################################################
+# Aliases
+
+set-alias vi 'C:\Program Files (x86)\vim\vim80\vim.exe'
+set-alias vim 'C:\Program Files (x86)\vim\vim80\vim.exe'
+set-alias man get-help -option allscope
+set-alias lsa ls-all -option allscope
+set-alias cdl cd-ls -option allscope 
+set-alias cls scroll-screen -option allscope
